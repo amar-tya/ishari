3e54e52d8fdf22520b3db52314f41aefc0aee777
amar-tya <ambrusdevelop@gmail.com>
2026-03-05 01:04:05 +0700

    fix: disable dark mode

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -7,7 +7,7 @@ export default function PublicLayout({
   readonly children: React.ReactNode;
 }) {
   return (
-    <div className="bg-[#f6f8f7] dark:bg-[#141e17] font-sans text-[#1e293b] antialiased min-h-screen flex flex-col overflow-x-hidden">
+    <div className="bg-[#f6f8f7] font-sans text-[#1e293b] antialiased min-h-screen flex flex-col overflow-x-hidden">
       <PublicNavbar />
       {children}
     </div>
